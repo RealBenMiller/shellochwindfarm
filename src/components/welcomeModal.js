e2d8939675ce3b7f784e9548524fe9442155db6b
RealBenMiller <b.r.miller@hotmail.co.uk>
2020-06-11 15:37:10 +0100

    adding form

--- src/components/welcomeModal.js
+++ src/components/welcomeModal.js
@@ -20,20 +20,13 @@ const WelcomeModal = () => {
           <h1>Welcome to the Exhibition</h1>
           <p>
             Welcome to the online consultation for Shelloch wind farm. We have
-            numbered each of the topics in suggested reading order, from 1 to
-            10. If you are reading them in order, you can click at the end of
-            each topic to take you to the next board. However, you are free to
-            look at the topics in any order you wish. When you have finished
-            reading a topic, click anywhere outside the window to return to this
-            main room. We hope that you will complete a questionnaire at the
-            end, so that we understand your views on the proposal.
-          </p>
-          <h2>How it works</h2>
-          <p>
-            To use this virtual exhibition, click on the numbered button on each
-            board. This will bring up the information contained. From there, you
-            can use the arrow keys to go directly to the next board, or you can
-            click the background to return to the main room.
+            numbered each of the topics in suggested reading order, from 1 to 8.
+            However, you are free to look at the topics in any order you wish.
+            You can navigate between topics using the arrows either side of the
+            content. When you have finished reading a topic, click anywhere
+            outside the window to return to this main room. We hope that you
+            will complete a questionnaire at the end, so that we understand your
+            views on the proposal.
           </p>
         </Modal.Body>
       </Modal>
