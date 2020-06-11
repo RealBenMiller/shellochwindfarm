6419e14d82663add6ba349f135caa7da5b064d0a
RealBenMiller <b.r.miller@hotmail.co.uk>
2020-06-11 14:33:56 +0100

    adding form

--- src/components/FeedbackModal.js
+++ src/components/FeedbackModal.js
@@ -28,7 +28,8 @@ const FeedbackModal = () => {
             it to us by email or post using the contact details at the end of
             the form.
           </p>
-          <Form>
+          <Form data-netlify="true" name="feedbackform" method="POST">
+            <input type="hidden" name="feedbackform" value="contact" />
             <Form.Group controlId="QuestionOne">
               <Form.Label
                 css={css`
