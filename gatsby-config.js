ca486a8bd0eaaf0ec44d22a154acfee41ec92175
RealBenMiller <b.r.miller@hotmail.co.uk>
2020-06-18 17:40:52 +0100

    adding Google Tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,14 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-emotion`,
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingID: "UA-127639939-2",
+        pageTransitionDelay: 0,
+        defer: false
+      }
+    }
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
